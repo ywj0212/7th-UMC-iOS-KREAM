3615e92731de7a698fbf76198aedbb953866f027
유우진 <ywj02120212@gmail.com>
2024-11-07 05:14:25 +0900

    SnapKit 포팅

--- UMC-iOS-KREAM/Views/LoginView.swift
+++ UMC-iOS-KREAM/Views/LoginView.swift
@@ -47,7 +47,7 @@ class LoginView: UIView {
         let button = UIButton(type: .system)
         button.setTitle("로그인", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18,weight: .medium)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         button.backgroundColor = .lightGray
         button.layer.cornerRadius = 8
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -58,7 +58,7 @@ class LoginView: UIView {
         let button = UIButton(type: .system)
         button.setTitle("카카오로 로그인", for: .normal)
         button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18,weight: .medium)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         button.backgroundColor = .white
         button.layer.borderWidth = 1
         button.layer.borderColor = CGColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
@@ -71,7 +71,7 @@ class LoginView: UIView {
         let button = UIButton(type: .system)
         button.setTitle("Apple로 로그인", for: .normal)
         button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18,weight: .medium)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         button.backgroundColor = .white
 //        button.setImage(UIImage(systemName: "apple.logo"), for: .normal)
         button.layer.borderWidth = 1
@@ -106,49 +106,51 @@ class LoginView: UIView {
     private func setupLayout() {
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([
-            self.topAnchor.constraint(equalTo: self.topAnchor),
-            self.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            self.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            
-            logoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 120),
-            logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            logoImageView.widthAnchor.constraint(equalToConstant: 287),
-            logoImageView.heightAnchor.constraint(equalToConstant: 75),
-            
-            idLabel.topAnchor.constraint(equalTo: topAnchor, constant: 250),
-            idLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            idLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            
-            idTextField.topAnchor.constraint(equalTo: idLabel.bottomAnchor, constant: 20),
-            idTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            idTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            idTextField.heightAnchor.constraint(equalToConstant: 40),
-            
-            pwdLabel.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 20),
-            pwdLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            pwdLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            
-            pwdTextField.topAnchor.constraint(equalTo: pwdLabel.bottomAnchor, constant: 20),
-            pwdTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            pwdTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            pwdTextField.heightAnchor.constraint(equalToConstant: 40),
-            
-            loginButton.topAnchor.constraint(equalTo: pwdTextField.bottomAnchor, constant: 30),
-            loginButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            loginButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            loginButton.heightAnchor.constraint(equalToConstant: 50),
-            
-            kakaoLoginButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 87),
-            kakaoLoginButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            kakaoLoginButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            kakaoLoginButton.heightAnchor.constraint(equalToConstant: 50),
-            
-            appleLoginButton.topAnchor.constraint(equalTo: kakaoLoginButton.bottomAnchor, constant: 22),
-            appleLoginButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            appleLoginButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            appleLoginButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(120)
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview().offset(287)
+            $0.height.equalTo(75)
+        }
+        idLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(250)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(idLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(40)
+        }
+        pwdLabel.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        pwdTextField.snp.makeConstraints {
+            $0.top.equalTo(pwdLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(40)
+        }
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(pwdTextField.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(50)
+        }
+        kakaoLoginButton.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(87)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(50)
+        }
+        appleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(kakaoLoginButton.snp.bottom).offset(22)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(50)
+        }
     }
 }
